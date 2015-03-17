0ccbb6c7c5bf05dc0a092f65127e5a6d8b27a719
Manoloide <manoloide@gmail.com>
2015-03-17 17:48:19 -0300

    first test set pixel and image surface

--- main.cpp
+++ main.cpp
@@ -3,7 +3,37 @@
 
 
 SDL_Window *window = NULL;
-SDL_Surface* screen = NULL;
+SDL_Surface *screen = NULL;
+SDL_Surface *image = NULL;
+
+bool quit = false;
+int amouseX = 0; 
+int amouseY = 0;
+int mouseX = 0;
+int mouseY = 0;
+
+
+void setPixel(SDL_Surface *Surface, int x, int y,Uint8 R, Uint8 G,Uint8 B)
+{
+
+    if( x < 0 || x >= Surface->w || y < 0 || y >= Surface->h)
+    {
+        return;
+    }
+    Uint32 color = SDL_MapRGB(Surface->format, R, G, B);
+    Uint8 *  bufp= (Uint8 *)Surface->pixels + y*Surface->pitch + x*Surface->format->BytesPerPixel;
+    switch (Surface->format->BytesPerPixel) {
+        case 4:
+        bufp[3] = color >> 24;
+        case 3:
+        bufp[2] = color >> 16;
+        case 2:
+        bufp[1] = color >> 8;
+        case 1:
+        bufp[0] = color;
+    }
+    return;
+}
 
 void init()
 {
@@ -15,9 +45,23 @@ void init()
 void close()
 {
 
+    SDL_FreeSurface(image);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
+}
+
+void render()
+{
+    //SDL_SetRenderDrawColor(image, 0xff, 0xff, 0xff, 0xff );
+    //SDL_RenderDrawLine(image, amouseY, amouseY, mouseX, mouseY);
+    setPixel(image, mouseX, mouseY, 255, 0, 0);
+    SDL_Rect srect;
+    srect.x = screen->w/2-image->w/2;
+    srect.y = screen->h/2-image->h/2;
+    srect.w = image->w;
+    srect.h = image->h;
+    SDL_BlitSurface( image, NULL, screen, &srect );
 }
 
 int main(int argc, char* argv[]) 
@@ -33,10 +77,9 @@ int main(int argc, char* argv[])
     {
 
         screen = SDL_GetWindowSurface( window );
-
-        bool quit = false;
-        int mouseX = 0;
-        int mouseY = 0;
+        SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 100, 100, 100));
+        image = SDL_CreateRGBSurface(0,200,200,32,0,0,0,0);
+        SDL_FillRect(image, NULL, SDL_MapRGB(image->format, 250, 250, 250));
         
         SDL_Event e;
 
@@ -50,8 +93,13 @@ int main(int argc, char* argv[])
                 {
                     quit = true;
                 }
+
+                amouseX = mouseX; 
+                amouseY = mouseY;
                 SDL_GetMouseState( &mouseX, &mouseY );
             }
+
+            render();
 
             SDL_UpdateWindowSurface( window );
             SDL_Delay(2);
