60b445db4e806b1615346dbdfb90d89b05102a54
Manoloide <manoloide@gmail.com>
2015-04-05 21:03:24 -0300

    fixed bug gradient no dist

--- view.cpp
+++ view.cpp
@@ -213,32 +213,32 @@ void View::update()
 			int y2 = my/scale;
 
 			float dd = dist(x1, y1, x2, y2);
-			if(radius)
+			if(dd >= 1)
 			{
-				for(int j = 0; j < canvas->h; j++){
-					for(int i = 0; i < canvas->w; i++){
-						float vv = (dd > 0)? dist(i, j, x1, y1)/dd : 1;
-						canvas->setPixel(i, j, lerpColor(c1, c2, vv));
+				if(radius)
+				{
+					for(int j = 0; j < canvas->h; j++){
+						for(int i = 0; i < canvas->w; i++){
+							float vv = (dd > 0)? dist(i, j, x1, y1)/dd : 1;
+							canvas->setPixel(i, j, lerpColor(c1, c2, vv));
+						}
+					}
+
+				}
+				if(linear)
+				{
+					float ang = atan2(y1-y2, x1-x2);
+					float x2 = x1 + cos(ang+M_PI/2)*dd;
+					float y2 = y1 + sin(ang+M_PI/2)*dd;
+					for(int j = 0; j < canvas->h; j++){
+						for(int i = 0; i < canvas->w; i++){
+							float d = ((x2-x1)*(j-y1)-(y2-y1)*(i-x1))/dd;
+							float v = d/dd;
+							canvas->setPixel(i, j, lerpColor(c1, c2, v));
+						}
 					}
 				}
-
 			}
-			if(linear)
-			{
-				float ang = atan2(y1-y2, x1-x2);
-				float x2 = x1 + cos(ang+M_PI/2)*dd;
-				float y2 = y1 + sin(ang+M_PI/2)*dd;
-				for(int j = 0; j < canvas->h; j++){
-					for(int i = 0; i < canvas->w; i++){
-						//float vv = (dd > 0)? dist(i, j, x1, y1)/dd : 1;
-						float d = ((x2-x1)*(j-y1)-(y2-y1)*(i-x1))/dd;
-						float v = d/dd;
-						canvas->setPixel(i, j, lerpColor(c1, c2, v));
-					}
-				}
-
-			}
-
 		}
 	}
 
